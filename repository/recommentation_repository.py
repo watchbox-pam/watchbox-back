6eb4fbc792516ff57c25c7ef72f05e95cd91b0c4
Adam <adamboissy2004@gmail.com>
2025-04-15 10:57:07 +0200

    add modif reco

--- repository/recommentation_repository.py
+++ repository/recommentation_repository.py
@@ -1,5 +1,5 @@
 from typing import List
-import random
+import itertools
 from domain.interfaces.i_recommendation_repository import IRecommendationRepository
 from domain.models.movie_list_item import MovieListItem
 from domain.models.emotion import Emotion, EMOTION_GENRE_MAPPING
@@ -9,22 +9,29 @@ from utils.tmdb_service import call_tmdb_api
 class RecommendationRepository(IRecommendationRepository):
     def get_by_emotion(self, emotion: Emotion, limit: int = 10) -> List[MovieListItem]:
         genre_ids = EMOTION_GENRE_MAPPING[emotion]
-
-        # Choose a random genre from those associated with the emotion
-        genre_id = random.choice(genre_ids)
-
-        # Get movies by genre
-        endpoint = f"/discover/movie?with_genres={genre_id}&language=fr-FR&sort_by=popularity.desc"
-        result = call_tmdb_api(endpoint)
-
         movies = []
-        if "results" in result:
-            for movie_data in result["results"][:limit]:
-                movie = MovieListItem(
-                    id=movie_data["id"],
-                    title=movie_data.get("title", ""),
-                    poster_path=movie_data.get("poster_path", "")
-                )
-                movies.append(movie)
 
-        return movies
+        for num_genres in range(len(genre_ids), 0, -1):
+            for genres_subset in itertools.combinations(genre_ids, num_genres):
+                genre_param = ",".join(map(str, genres_subset))
+                endpoint = f"/discover/movie?with_genres={genre_param}&language=fr-FR&sort_by=popularity.desc"
+                result = call_tmdb_api(endpoint)
+
+                if "results" in result and result["results"]:
+                    for movie_data in result["results"]:
+                        if len(movies) >= limit:
+                            return movies
+
+                        movie = MovieListItem(
+                            id=movie_data["id"],
+                            title=movie_data.get("title", ""),
+                            poster_path=movie_data.get("poster_path", "")
+                        )
+
+                        if not any(m.id == movie.id for m in movies):
+                            movies.append(movie)
+
+            if len(movies) >= limit:
+                return movies
+
+        return movies[:limit]
